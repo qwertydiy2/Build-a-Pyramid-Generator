dd045999593d9b5e2739a72bcf054d506fb9ac01
qwertydiy2 <reneyibowei@sacredheart.southwark.sch.uk>
2024-06-30 14:34:08 +0100

    feat: Implemented dynamic pyramid generation with CSS styling

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
 function generatePyramid() {
   const height = document.getElementById('height').value;
-  let pyramid = '';
-  for (let i = 0; i < height; i++) {
-    pyramid += ' '.repeat(height - i - 1) + '#'.repeat(2 * i + 1) + '\n';
+  const pyramidContainer = document.getElementById('pyramid-container');
+
+  // Clear the pyramid container
+  pyramidContainer.innerHTML = '';
+
+  for (let i = 1; i <= height; i++) {
+    // Create a new pyramid layer
+    const pyramidLayer = document.createElement('div');
+    pyramidLayer.className = 'pyramid-layer';
+
+    // Create pyramid blocks
+    for (let j = 0; j < i; j++) {
+      const pyramidBlock = document.createElement('span');
+      pyramidBlock.className = 'pyramid-block';
+      pyramidLayer.appendChild(pyramidBlock);
+    }
+
+    // Append the pyramid layer to the pyramid container
+    pyramidContainer.appendChild(pyramidLayer);
   }
-  document.getElementById('pyramidDisplay').innerText = pyramid;
 }
